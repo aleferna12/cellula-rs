2ecff27d7e7035dcdb734de02e872cfe51d1abab
aleferna12 <aleferna2001@hotmail.com>
2025-08-12 16:08:54 +0100

    Weighted selection test

--- src/ecology/selector.rs
+++ src/ecology/selector.rs
@@ -1,5 +1,5 @@
-use rand::Rng;
 use crate::cell::Fit;
+use rand::Rng;
 
 pub trait Selector {
     // This can instead return an impl Iterator<Item = F> if needed for generalisation
@@ -43,9 +43,36 @@ impl<R: Rng> Selector for WeightedSelection<R> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::SeedableRng;
+    use rand_xoshiro::Xoshiro256StarStar;
+    use std::collections::HashSet;
+
+    #[derive(Debug, Hash, PartialEq, Eq, Copy, Clone)]
+    struct FitTest(usize);
+
+    impl Fit for FitTest {
+        fn fitness(&self) -> f32 {
+            self.0 as f32
+        }
+    }
     
     #[test]
     fn test_weighted_selection() {
-        
+        let fits: [FitTest; 100] = core::array::from_fn(|i| FitTest(i));
+        let fit_sum = fits.iter().map(|f| f.fitness()).sum::<f32>();
+        assert_eq!(fit_sum, 4950.);
+
+        for seed in 0..100 {
+            let mut sel = WeightedSelection {
+                select_n: 100,
+                rng: Xoshiro256StarStar::seed_from_u64(seed)
+            };
+            let new_fits = sel.select(&fits);
+            let new_fit_sum = new_fits.iter().map(|f| f.fitness()).sum::<f32>();
+            assert!(new_fit_sum > fit_sum);
+
+            let unique: HashSet<FitTest> = new_fits.into_iter().copied().collect();
+            assert!(unique.len() < sel.select_n as usize);
+        }
     }
 }
