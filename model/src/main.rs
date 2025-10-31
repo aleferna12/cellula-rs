6d3215bd6621d39f36bbc793f9321132f1039e3f
aleferna <aleferna2001@hotmail.com>
2025-10-31 06:23:57 +0000

    CAREFUL PAST THIS COMMIT! I didnt know about cherry-picking, so a lot of commits below show as "merged" even though I discarded all changes (forgive me >_<)

--- model/src/main.rs
+++ model/src/main.rs
@@ -1,6 +1,6 @@
 /*
 TODO!:
-    - strip down of non essential features (mom, chem, ...)
+    -
     - profile data write, something is taking awfully long (prob lattice write)
  */
 use clap::Parser;
