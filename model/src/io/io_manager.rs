cebf3200e9b4eb022bcbea1f22e99566b98cfb50
aleferna <aleferna2001@hotmail.com>
2025-11-06 02:40:47 +0000

    Changed dangerous wildcard imports
    # Conflicts:
    #	model/src/io/io_manager.rs
    #	model/tests/model.rs

--- model/src/io/io_manager.rs
+++ model/src/io/io_manager.rs
@@ -1,10 +1,10 @@
 use crate::cell::Cell;
-use crate::my_environment::MyEnvironment;
 use crate::evolution::grn::{EdgeWeight, Grn, GrnGeneType};
 use crate::io::movie_maker::MovieMaker;
 use crate::io::node_link::{GrnMutParams, NodeLinkData};
 use crate::io::parameters::Parameters;
-use crate::io::plot::*;
+use crate::io::plot::Plot;
+use crate::my_environment::MyEnvironment;
 use anyhow::{anyhow, bail, Context};
 use bon::Builder;
 use cellulars_lib::basic_cell::{BasicCell, Cellular, RelCell};
--- model/tests/model.rs
+++ model/tests/model.rs
@@ -1,14 +1,13 @@
 use cellulars_lib::step::Step;
-use model::io::parameters::Parameters;
-use model::io::parameters::PlotType::*;
+use model::io::parameters::{Parameters, PlotType as PT};
 use model::model::Model;
 
 #[test]
 fn test_run() -> anyhow::Result<()> {
-    for plot in [CellType, Area, Center, ChemCenter] {
+    for plot in [PT::CellType, PT::Area, PT::Center, PT::ChemCenter] {
         let mut params = Parameters::parse("examples/64_cells.toml")?;
-        params.io.outdir = format!("tests/{plot:?}").into();
-        params.io.plot.order = vec![Chem, Spin, plot, Border];
+        params.io.outdir = format!("tests/{plot:?}");
+        params.io.plot.order = vec![PT::Chem, PT::Spin, plot, PT::Border];
         params.io.image_period = 64;
         params.io.movie.show = false;
         params.cell.update_period = 1;
@@ -18,7 +17,7 @@ fn test_run() -> anyhow::Result<()> {
         model.io.write_if_time(4096, &model.pond.env)?;
         
         let sim_dir = params.io.outdir.clone();
-        params.io.outdir = params.io.outdir + "/resumed/";
+        params.io.outdir += "/resumed/";
         let mut res_model = Model::initialise_from_backup(params, sim_dir, 4096)?;
         res_model.run_for(128);
     }
