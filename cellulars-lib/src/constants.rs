57785d024f99ffe151da5ad53678051b2861ec5b
aleferna <aleferna2001@hotmail.com>
2026-01-06 20:31:55 +0000

    Better docs

--- cellulars-lib/src/constants.rs
+++ cellulars-lib/src/constants.rs
@@ -4,7 +4,10 @@
 pub type CellIndex = u32;
 
 /// Type of the floats used throughout the simulation.
-/// 
+///
+/// If the `high-precision` feature is enabled (the default), this will be [f64].
+/// Otherwise, [f32] will be used.
+///
 /// Using [f32] instead if [f64] provides some performance gain at the cost of mathematical precision.
 #[cfg(feature = "high-precision")]
 pub type FloatType = f64;
