efcde0c17cc2dc6f947fb09873373157a13b3db1
aleferna12 <aleferna2001@hotmail.com>
2026-01-22 17:40:58 +0000

    Better tests

--- model/src/kinect.rs
+++ model/src/kinect.rs
@@ -10,5 +10,7 @@ unsafe extern "C" {
     pub fn kinect_listen_frame(h: *mut KinectHandle, ms: i32) -> bool;
     pub fn kinect_release_frame(h: *mut KinectHandle);
     pub fn kinect_destroy(h: *const KinectHandle);
+    pub fn kinect_color(h: *mut KinectHandle) -> *const u8;
     pub fn kinect_depth(h: *mut KinectHandle) -> *const f32;
+    pub fn kinect_ir(h: *mut KinectHandle) -> *const f32;
 }
--- model/tests/kinect.rs
+++ model/tests/kinect.rs
@@ -1,4 +1,4 @@
-use image::Luma;
+use image::{Luma, Rgb};
 use imageproc::definitions::Image;
 use model::kinect::*;
 
@@ -9,13 +9,15 @@ fn test_kinect() {
         assert!(!h.is_null());
 
         assert!(kinect_listen_frame(h, 10_000));
-        let data = kinect_depth(h);
-        let data_arr = std::ptr::read(data as *const [f32; 512 * 424]);
-        let image = Image::from_fn(
+
+        let depth_data = kinect_depth(h);
+        assert!(!depth_data.is_null());
+        let depth_slice = std::slice::from_raw_parts(depth_data, 512 * 424);
+        let depth_image = Image::from_fn(
             512,
             424,
             |i, j| {
-                let mut val = data_arr[(j * 512 + i) as usize];
+                let mut val = depth_slice[(j * 512 + i) as usize];
                 if val != 0. && !val.is_normal() {
                     val = 0.;
                 }
@@ -23,9 +25,40 @@ fn test_kinect() {
                 Luma([(val / 20.) as u8])
             },
         );
-        dbg!(&image);
+
+        let ir_data = kinect_ir(h);
+        assert!(!ir_data.is_null());
+        let ir_slice = std::slice::from_raw_parts(ir_data, 512 * 424);
+        let ir_image = Image::from_fn(
+            512,
+            424,
+            |i, j| {
+                let mut val = ir_slice[(j * 512 + i) as usize];
+                if val != 0. && !val.is_normal() {
+                    val = 0.;
+                }
+                // Dividing by 100 as a hack to get more values into the appropriate range
+                Luma([(val / 100.) as u8])
+            },
+        );
+
+        let color_data = kinect_color(h);
+        assert!(!color_data.is_null());
+        let color_slice = std::slice::from_raw_parts(color_data as *const [u8; 4], 1920 * 1080);
+        let color_image = Image::from_fn(
+            1920,
+            1080,
+            |i, j| {
+                let val = color_slice[(j * 1920 + i) as usize];
+                Rgb([val[2], val[1], val[0]])
+            },
+        );
+
         kinect_release_frame(h);
         kinect_destroy(h);
-        image.save("tests/test_kinect.png").unwrap();
+
+        depth_image.save("tests/test_kinect_depth.png").unwrap();
+        ir_image.save("tests/test_kinect_ir.png").unwrap();
+        color_image.save("tests/test_kinect_rgb.png").unwrap();
     }
 }
