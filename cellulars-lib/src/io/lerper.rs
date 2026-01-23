039bfe9acbfae5ac0f4dd6cbe9840fa3c21d79cf
aleferna12 <aleferna2001@hotmail.com>
2026-01-23 18:21:05 +0000

    Started moving plot logic to cellulars-lib

---
+++ cellulars-lib/src/io/lerper.rs
@@ -0,0 +1,41 @@
+use num::{Num, Zero};
+use palette::Mix;
+
+/// Used to interpolate colors with [`Lerper::lerp()`].
+pub struct Lerper<C> {
+    pub min_color: C,
+    pub max_color: C
+}
+
+impl<C> Lerper<C> {
+    /// Linearly interpolates `value` between `min` and `max`.
+    pub fn lerp(&self, value: C::Scalar, min: C::Scalar, max: C::Scalar) -> Result<C, LerpError>
+    where
+        C: Mix + Clone,
+        C::Scalar: Num + PartialOrd + Clone {
+        if max < min {
+            return Err(LerpError::NegativeRange);
+        }
+        if value < min {
+            return Err(LerpError::ValueTooSmall);
+        }
+        if value > max {
+            return Err(LerpError::ValueTooLarge);
+        }
+
+        let p = if min == max { C::Scalar::zero() } else { (value - min.clone()) / (max - min) };
+        let blended = self.min_color.clone().mix(self.max_color.clone(), p);
+        Ok(blended)
+    }
+}
+
+/// Error thrown when linear interpolation fails.
+#[derive(Debug)]
+pub enum LerpError {
+    /// Value falls outside the range because it's too small.
+    ValueTooSmall,
+    /// Value falls outside the range because it's too large.
+    ValueTooLarge,
+    /// Minimum value passed is larger than maximum.
+    NegativeRange
+}
---
+++ cellulars-lib/src/io/plot.rs
@@ -0,0 +1,155 @@
+use crate::constants::{CellIndex, FloatType};
+use crate::prelude::{Cellular, Habitable};
+use crate::spin::Spin;
+use image::{Rgba, RgbaImage};
+use palette::{IntoColor, Mix, Srgba};
+use std::hash::{DefaultHasher, Hash, Hasher};
+use imageproc::drawing::draw_cross_mut;
+use crate::io::lerper::Lerper;
+
+/// A trait to plot information about the simulation.
+pub trait Plot<P> {
+    /// Plots the information in `plottable` by drawing on `image`.
+    fn plot(&self, plottable: &P, image: &mut RgbaImage);
+}
+
+/// Plots the spin of cells in random colors (except for the solid and medium spin colors, which can be chosen).
+pub struct SpinPlot {
+    /// Color used for [`Spin::Solid`].
+    pub solid_color: Srgba<u8>,
+    /// Color used for [`Spin::Medium`].
+    pub medium_color: Option<Srgba<u8>>
+}
+
+impl SpinPlot {
+    fn cell_index_to_rgb(index: CellIndex) -> Srgba<u8> {
+        let mut hasher = DefaultHasher::new();
+        index.hash(&mut hasher);
+        let hashed = hasher.finish();
+        Srgba::new(
+            (hashed & 0xFF) as u8,
+            (hashed >> 8 & 0xFF) as u8,
+            (hashed >> 16 & 0xFF) as u8,
+            255
+        )
+    }
+}
+
+impl<E: Habitable> Plot<E> for SpinPlot {
+    fn plot(&self, env: &E, image: &mut RgbaImage) {
+        for pos in env.env().cell_lattice.iter_positions() {
+            let spin = env.env().cell_lattice[pos];
+            let rgba = match spin {
+                Spin::Some(cell_index) => Some(Self::cell_index_to_rgb(cell_index)),
+                Spin::Solid => Some(self.solid_color),
+                Spin::Medium => self.medium_color,
+            };
+            if let Some(color) = rgba {
+                image.put_pixel(pos.x as u32, pos.y as u32, srgba_to_rgba(color));
+            }
+        }
+    }
+}
+
+/// Plots the center of cells.
+pub struct CenterPlot {
+    /// Color of the cell center.
+    pub color: Srgba<u8>
+}
+
+impl<E: Habitable> Plot<E> for CenterPlot {
+    fn plot(&self, env: &E, image: &mut RgbaImage) {
+        for rel_cell in env.env().cells.iter() {
+            if !rel_cell.cell.is_empty() {
+                continue;
+            }
+            let center = rel_cell
+                .cell
+                .center()
+                .round();
+            draw_cross_mut(image, srgba_to_rgba(self.color), center.x as i32, center.y as i32);
+        }
+    }
+}
+
+/// Plots the border of cells.
+pub struct BorderPlot {
+    /// Color of the border.
+    pub color: Srgba<u8>
+}
+
+impl<E: Habitable> Plot<E> for BorderPlot {
+    fn plot(&self, env: &E, image: &mut RgbaImage) {
+        for pos in env.env().cell_lattice.iter_positions() {
+            let spin = env.env().cell_lattice[pos];
+            let Spin::Some(cell_index) = spin else {
+                continue;
+            };
+
+            let is_border = env
+                .env()
+                .valid_neighbors(pos)
+                .any(|neigh| {
+                    let neigh_spin = env.env().cell_lattice[neigh];
+                    match neigh_spin {
+                        Spin::Some(neigh_index) => cell_index < neigh_index,
+                        _ => true
+                    }
+                });
+            if is_border {
+                image.put_pixel(pos.x as u32, pos.y as u32, srgba_to_rgba(self.color));
+            }
+        }
+    }
+}
+
+/// Plots cell area.
+pub struct AreaPlot<C> {
+    lerper: Lerper<C>
+}
+
+impl<E, C> Plot<E> for AreaPlot<C>
+where
+    E: Habitable,
+    C: Mix<Scalar = FloatType>
+        + Clone
+        + IntoColor<Srgba> {
+    fn plot(&self, env: &E, image: &mut RgbaImage) {
+        let mut min = u32::MAX;
+        let mut max = 0;
+        for rel_cell in env.env().cells.iter() {
+            if !rel_cell.cell.is_empty() {
+                continue;
+            }
+            if rel_cell.cell.area() < min {
+                min = rel_cell.cell.area()
+            }
+            if rel_cell.cell.area() > max {
+                max = rel_cell.cell.area()
+            }
+        }
+
+        for pos in env.env().cell_lattice.iter_positions() {
+            if let Spin::Some(cell_index) = env.env().cell_lattice[pos] {
+                let rel_cell = &env.env().cells[cell_index];
+                let color = self.lerper.lerp(
+                    rel_cell.cell.area() as FloatType,
+                    min as FloatType,
+                    max as FloatType
+                );
+                match color {
+                    Ok(c) => image.put_pixel(
+                        pos.x as u32,
+                        pos.y as u32,
+                        srgba_to_rgba(c.into_color().into())
+                    ),
+                    Err(e) => log::warn!("Failed to plot area for pos `{pos:?}` with error `{e:?}`")
+                };
+            }
+        }
+    }
+}
+
+fn srgba_to_rgba(srgba: Srgba<u8>) -> Rgba<u8> {
+    Rgba([srgba.red, srgba.green, srgba.blue, srgba.alpha])
+}
