3b533b1fb38200be727abd664039276763b9ebe0
aleferna12 <aleferna2001@hotmail.com>
2026-03-03 18:43:38 +0000

    Renamed feature

--- cellulars/src/constants.rs
+++ cellulars/src/constants.rs
@@ -5,7 +5,7 @@ pub type CellIndex = u32;
 
 /// Type of the floats used throughout the simulation.
 ///
-/// If the `high-precision` feature is enabled (the default), this will be [f64].
+/// If the `f64` feature is enabled (the default), this will be [f64].
 /// Otherwise, [f32] will be used.
 ///
 /// Using [f32] instead if [f64] provides some performance gain at the cost of mathematical precision.
