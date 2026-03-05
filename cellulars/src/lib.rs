633f58c99012b5b944b729e7dcd1d8f344205737
aleferna12 <aleferna2001@hotmail.com>
2026-03-05 17:35:15 +0000

    Readme update

--- cellulars/src/lib.rs
+++ cellulars/src/lib.rs
@@ -4,12 +4,12 @@ A very fast, extensible CPM implementation.
 ## Getting started:
 
 A minimal CPM implementation can be found in the
-[basics example](https://github.com/aleferna12/cellula-rs/blob/master/cellulars/examples/basic.rs),
+[basic example](https://github.com/aleferna12/cellula-rs/blob/master/cellulars/examples/basic.rs),
 which should give you an idea of how to get started.
 
 Also check out the other [examples](https://github.com/aleferna12/cellula-rs/blob/master/cellulars/examples).
 
-Finally, we provide a [template repo](https://github.com/aleferna12/cellulars-template)
+Alternatively, we provide a [template repo](https://github.com/aleferna12/cellulars-template)
 with additional features that do not fit in the library to reduce boilerplate.
 These include a command line tool, a parameter parser and automatic data-write/backup.
 */
