e9e884ae798567cf68c29b7010ef6ac6a7c621ca
aleferna12 <aleferna2001@hotmail.com>
2026-04-06 17:26:42 +0100

    Updated plots

--- analyses/analyses/phylogeny.py
+++ analyses/analyses/phylogeny.py
@@ -1,4 +1,6 @@
 import polars as pl
+from ete3 import Tree
+
 
 def matrix(celldf, season_duration):
     wtimes = celldf["wtime"].sort().diff()
@@ -26,8 +28,35 @@ def matrix(celldf, season_duration):
                 )
                 .select(["index", "index_right", pl.col(r"^\d*$")])
                 .rename({
-                    "index_right": f"{time - int(season_duration + wtime)}"
+                    "index_right": f"{time - int(season_duration)}"
                 })
         )
-    ancdf = ancdf.rename({"index": str(time - int(wtime))})
+    ancdf = ancdf.rename({"index": str(time)})
     return ancdf
+
+
+def trees(phy_matrix):
+    prev_time = "0"
+    nodes = [None] * phy_matrix[prev_time].n_unique()
+    for x in phy_matrix[prev_time].unique():
+        t = Tree(name=str(x))
+        t.add_feature("time", "0")
+        nodes[x] = t
+    for row in phy_matrix.transpose():
+        row = row[::-1]
+        prev_anc = row[0]
+        node = nodes[prev_anc]
+        for anc in row.filter(row.is_not_null())[1:]:
+            anc_str = str(anc)
+            found = False
+            for child in node.children:
+                if child.name == anc_str:
+                    found = True
+                    break
+            if found:
+                node = child
+            else:
+                time = str(2_000_000 + int(node.time))
+                node = node.add_child(name=anc_str)
+                node.add_feature("time", time)
+    return nodes
